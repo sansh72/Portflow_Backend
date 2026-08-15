e064214cefeab95426d16e26d9a14506587e2ecd
Sansh <sanshraysinghlangeh@gmail.com>
2026-08-16 05:09:52 +0530

    added 2 more urls to allow hitpoint

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175",
         "https://www.portflow.co.in",
         "https://portflow.co.in",
     ],
